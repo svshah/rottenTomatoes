2502fe1dd468fe41028ac40692799a242ef2d451
Sameer Shah <ssameer@yahoo-inc.com>
2015-01-26 18:59:15 -0800

    Initial Commit

---
+++ rottenTomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rottenTomatoes
+//
+//  Created by Sameer Shah on 1/26/15.
+//  Copyright (c) 2015 Sameer Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rottenTomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rottenTomatoes
+//
+//  Created by Sameer Shah on 1/26/15.
+//  Copyright (c) 2015 Sameer Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
